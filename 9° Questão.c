8741a7ca9769dae27a2777462ce7f4219c0dd60b
Arthur Sean C. Campos <113319924+arthursean@users.noreply.github.com>
2023-04-04 22:31:43 -0300

    Update 9° Questão.c

--- 9° Questão.c
+++ 9° Questão.c
@@ -1,85 +1,55 @@
 #include <stdio.h>
-
-// Função para calcular o inverso modular de a mod m
-int modular_inverse(int a, int m)
-{
-    int m0 = m, s = 0, t = 1, num, q;
- 
-    if (m == 1)
-        return 0;
- 
-    while (m)
-    {
-        q = a / m;
-        num = m;
-        m = a % m, a = num;
-        num = s;
-        s = t - q * s;
-        t = num;
-    }
- 
-    if (s < 0)
-        s += m0;
-    
-    while(s<=0)
-    {
-        s+=m0;
-    }
-    return s;
-}
-
-// Função para verificar se um número é primo
-int coprimo(int a, int b){
-    if(b==0)
-    {
+int gcd(int a, int b) {
+    if (b == 0) {
         return a;
     }
-    else
-    {
-        return coprimo(b, a%b);
-    }
+    return gcd(b, a % b);
 }
 
-// Função para calcular a solução usando o Teorema Chinês do Resto
-int chinese_remainder(int a[], int m[], int n)
-{
-    int indice, nzao = 1, sum = 0, p;
-
-    // Calcular a multiplicação de todos os valores de m
-    for (indice = 0; indice < n; indice++) {
-        nzao = nzao * m[indice];
+int extended_euclidean(int a, int b, int *x, int *y) {
+    if (b == 0) {
+        *x = 1;
+        *y = 0;
+        return a;
     }
-
-    // Calcula a solução
-    for (indice = 0; indice < n; indice++) {
-        p = nzao / m[indice];
-        sum += a[indice] * modular_inverse(p, m[indice]) * p;
-    }
-
-    return sum % nzao;
+    int x1, y1;
+    int gcd = extended_euclidean(b, a % b, &x1, &y1);
+    *x = y1;
+    *y = x1 - (a / b) * y1;
+    return gcd;
 }
 
-int main()
-{
-    int a[3], m[3], indice;
-
-    printf("Digite as tres congruencias (no formato x = a[i] (mod m[i])), com 'i' sendo indice:\n");
-
-    for (indice = 0; indice < 3; indice++) {
-        printf("a[valor da %d congruencia(s)] = \n", indice+1);
-        scanf("%d", &a[indice]);
-        printf("m[valor da %d congruencia(s)] = \n", indice+1);
-        scanf("%d", &m[indice]);
+int mod_inverse(int a, int m) {
+    int x, y;
+    int gcd = extended_euclidean(a, m, &x, &y);
+    if (gcd != 1) {
+        return -1; // a e m nao sao coprimos
     }
+    return (x % m + m) % m;
+}
 
+int chinese_remainder(int b1, int m1, int b2, int m2, int b3, int m3) {
+    int M = m1 * m2 * m3;
+    int x = b1 * mod_inverse(M / m1, m1) * M / m1+
+            b2 * mod_inverse(M / m2, m2) * M/m2 +
+            b3 * mod_inverse(M / m3, m3) * M/m3;
+    return x % M;
+}
 
-    // Verifica se todos os valores de m são primos
-    if (!coprimo(coprimo(m[indice], m[indice+1]), m[indice+2])) {
-        printf("Os modulos devem ser primos entre si!\n");
+int main() {
+    int a1,b1, b2, b3, m1, a2, m2, a3, m3;
+    printf("Digite as tres congruencias no formato 'ax = b (mod m)':\n");
+    scanf("%d x = %d (mod %d)", &a1, &b1, &m1);
+    scanf("%d x = %d (mod %d)", &a1, &b2, &m2);
+    scanf("%d x = %d (mod %d)", &a1, &b3, &m3);
+
+    if (gcd(m1, gcd(m2, m3)) != 1) {
+        printf("Os modulos nao sao coprimos. Nao ha solucao unica.\n");
         return 0;
     }
 
-    printf("A solucao unica das tres congruencias eh: %d\n", chinese_remainder(a, m, 3));
+    int x = chinese_remainder(b1, m1, b2, m2, b3, m3);
+    printf("A solucao unica da congruencia eh: %d\n", x);
 
     return 0;
 }
